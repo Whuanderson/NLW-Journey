f4ca84bec8be784ffb04eec48701742ddc028bb7
Whuanderson Marinho <whuandersonmsn@hotmail.com>
2024-07-10 15:17:35 -0300

    aula 1 done

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,7 +1,10 @@
+import { createContext, useContext } from "react";
+
 import {
   Text,
-  View,
   TextProps,
+  View,
+  ActivityIndicatorBase,
   TouchableOpacity,
   TouchableOpacityProps
 } from "react-native";
@@ -14,6 +17,8 @@ type ButtonProps = TouchableOpacityProps & {
   isLoading?: boolean
 }
 
+const ThemeContext = createContext<{ variant?: Variants }>({});
+
 function Button({
   variant = "primary",
   children,
@@ -23,27 +28,38 @@ function Button({
 }: ButtonProps) {
   return (
     <TouchableOpacity
+      activeOpacity={0.7}
+      disabled={isLoading}
+      {...rest}
     >
       <View
-      className={clsx(
-        "h-11 flex-row items-center justify-center rounded-lg gap-2 px-2",
-        {
-          "bg-lime-300": variant === "primary",
-          "bg-zinc-800": variant === "secondary",
-        },
-        className
-      )}
-      {...rest}      
+        className={clsx(
+          "h-11 flex-row items-center justify-center rounded-lg gap-2 px-2",
+          {
+            "bg-lime-300": variant === "primary",
+            "bg-zinc-800": variant === "secondary",
+          },
+          className
+        )}
       >
-        {children}
+        <ThemeContext.Provider value={{ variant }}>
+          {isLoading ? <ActivityIndicatorBase className="text-lime-950" /> : children}
+        </ThemeContext.Provider>
       </View>
     </TouchableOpacity>
   )
 }
 
 function Title({ children }: TextProps) {
+  const { variant } = useContext(ThemeContext);
   return (
-    <Text>{children}</Text>
+    <Text className={clsx(" text-base  font-semibold", {
+      "text-lime-950": variant === "primary",
+      "text-zinc-200": variant === "secondary",
+    })}
+    >
+      {children}
+    </Text>
   )
 }
 
--- src/components/input.tsx
+++ src/components/input.tsx
@@ -1,5 +1,10 @@
 import { ReactNode } from "react";
-import { TextInput, TextInputProps, View, Platform } from "react-native";
+import { 
+  TextInput, 
+  TextInputProps, 
+  View, 
+  Platform 
+} from "react-native";
 import clsx from "clsx";
 
 import { colors } from "@/styles/colors";
@@ -13,10 +18,11 @@ type InputProps = {
 
 function Input({ children, variant = "primary" }: InputProps) {
   return (
-    <View className={clsx("w-full h-16 flex-row items-center gap-2", {
+    <View 
+      className={clsx("w-full h-16 bg-transparent flex-row items-center gap-2", {
         "h-14 px-4 rounded-lg border border-zinc-800": variant !== "primary",
-        "bg-zinc-950": variant !== "secondary",
-        "bg-zinc-900": variant !== "tertiary",
+        "bg-zinc-950": variant === "secondary",
+        "bg-zinc-900": variant === "tertiary",
       })}
     >
       {children}
